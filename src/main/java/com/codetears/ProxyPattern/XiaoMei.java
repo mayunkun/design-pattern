5628a82b9e282f74a454a0ca62577e1434d0d9e1
mayunkun <1163363720@qq.com>
2019-10-11 15:44:21 +0800

    代理模式

---
+++ src/main/java/com/codetears/ProxyPattern/XiaoMei.java
@@ -0,0 +1,18 @@
+package com.codetears.ProxyPattern;
+
+/**
+ * @author l'amour solitaire
+ * @description 小梅(被追求者)
+ * @date 2019/10/11 下午3:14
+ */
+public class XiaoMei {
+    // 姓名
+    private String name;
+    // 年龄
+    private Integer age;
+
+    public XiaoMei(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/codetears/ProxyPattern/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author l'amour solitaire
+ * @description 代理模式
+ * @date 2019/10/11 下午3:03
+ */
+package com.codetears.ProxyPattern;
