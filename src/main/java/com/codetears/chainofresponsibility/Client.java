88a9b93e128ab04f9d09b611b137d9f6d4c1e36a
mayunkun <1163363720@qq.com>
2019-10-12 14:23:52 +0800

    阿里代码规范

--- src/main/java/com/codetears/chainofresponsibility/Client.java
+++ src/main/java/com/codetears/chainofresponsibility/Client.java
@@ -5,12 +5,12 @@ import java.math.BigDecimal;
 /**
  * @author l'amour solitaire
  * @description TODO
- * @date 2019/10/12 下午2:20 
+ * @date 2019/10/12 下午2:20
  */
 public class Client {
 
     public static void main(String[] args) throws InterruptedException {
-        Manager bossManager = new BossManager("老板"), ctoManager = new CtoManager(bossManager, "总监"), commonManager = new CommonManager(ctoManager, "经理");
+        AbstractManager bossManager = new BossManager("老板"), ctoManager = new CtoManager(bossManager, "总监"), commonManager = new CommonManager(ctoManager, "经理");
 
         Request request = new Request();
         request.setType(1);
